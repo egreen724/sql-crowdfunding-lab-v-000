f517da4506ea66441ba2ee9c3a31a4f846f11da9
Erin Green <erin.y.green@gmail.com>
2018-11-15 19:07:55 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, pledges.pledge_amount FROM projects 
-JOIN projects ON pledges 
+JOIN pledges ON pledges 
 
 "
 end
