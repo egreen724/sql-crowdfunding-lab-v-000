71a9c000c9981d93d6ca804e9aad0584531fe20e
Erin Green <erin.y.green@gmail.com>
2018-11-15 19:23:02 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,12 @@ ORDER BY users.name ASC;
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, "
+"SELECT projects.title, (SUM(pledges.amount) - SUM(projects.goal)) from projects
+JOIN pledges
+ON projects.id = pledges.project_id
+WHERE projects.goal < pledges.amount
+GROUP BY projects 
+"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
