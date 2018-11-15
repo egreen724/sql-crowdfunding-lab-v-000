cd8d27a48aa731cb421bd97f5dc81ca480640de1
Erin Green <erin.y.green@gmail.com>
2018-11-15 19:24:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 JOIN pledges
 ON projects.id = pledges.project_id
 WHERE projects.funding_goal < pledges.amount
-GROUP BY projects; 
+GROUP BY projects.title; 
 "
 end
 
