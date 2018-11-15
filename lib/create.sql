7e174f75b6809f16a00fbd7623ec21e8be402ef2
Erin Green <erin.y.green@gmail.com>
2018-11-15 18:44:23 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE projects (
-id PRIMARY KEY, 
+id INTEGER PRIMARY KEY, 
 title TEXT,
 category TEXT,
 start_date TEXT,
@@ -7,13 +7,13 @@ end_date TEXT)
 ;
 
 CREATE TABLE users (
-  id PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
 );
 
 CREATE TABLE pledges (
-  id PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   amount INTEGER,
   user_id INTEGER,
   project_id INTEGER
